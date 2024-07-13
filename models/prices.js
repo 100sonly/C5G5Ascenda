0fd901599820d0c67ccda7623de4353f933d38d9
FooChuanTian <foochuantian@gmail.com>
2024-07-13 19:58:35 +0800

    Completed search for price by hotel backend

--- models/prices.js
+++ models/prices.js
@@ -10,19 +10,49 @@ class Price {
     }
 }
 
+class Room {
+    constructor(roomNormalizedDescription, free_cancellation, description, long_description, images, amenities, price, market_rates) {
+        this.roomNormalizedDescription = roomNormalizedDescription;
+        this.free_cancellation = free_cancellation;
+        this.description = description;
+        this.long_description = long_description;
+        this.images = images;
+        this.amenities = amenities;
+        this.price = price;
+        this.market_rates = market_rates;
+    }
+    static newRoom(roomNormalizedDescription, free_cancellation, description, long_description, images, amenities, price, market_rates) {
+        return new Room(roomNormalizedDescription, free_cancellation, description, long_description, images, amenities, price, market_rates);
+    }
+}
+
 async function pricesByDestination(destination_id, checkin, checkout, lang, currency, guests, partner_id) {
     const response = await fetch(`https://hotelapi.loyalty.dev/api/hotels/prices?destination_id=${destination_id}&checkin=${checkin}&checkout=${checkout}&lang=${lang}&currency=${currency}&guests=${guests}&partner_id=${partner_id}`);
     const text = await response.text();
     const text_json = await JSON.parse(text);
-    const rooms = text_json['hotels'];
+    const hotels = text_json['hotels'];
     //console.log(Object.keys(text_json));
     var price_list = [];
-    for (let i=0; i < rooms.length; i++) {
-        current = rooms[i];
+    for (let i=0; i < hotels.length; i++) {
+        current = hotels[i];
         prices_to_add = new Price(current.id, current.searchRank, current.price, current.market_rates);
         price_list.push(prices_to_add);
     }
     return price_list;
 }
 
-module.exports = { Price, pricesByDestination }
+async function priceByRoom(hotel_id, destination_id, checkin, checkout, lang, currency, guests, partner_id) {
+    const response = await fetch(`https://hotelapi.loyalty.dev/api/hotels/${hotel_id}/price?destination_id=${destination_id}&checkin=${checkin}&checkout=${checkout}&lang=${lang}&currency=${currency}&guests=${guests}&partner_id=${partner_id}`);
+    const text = await response.text();
+    const text_json = await JSON.parse(text);
+    const rooms = text_json['rooms'];
+    var room_list = [];
+    for (let i=0; i < rooms.length; i++) {
+        current = rooms[i];
+        room_to_add = new Room(current.roomNormalizedDescription, current.free_cancellation, current.description, current.long_description, current.images, current.amenities, current.price, current.market_rates);
+        room_list.push(room_to_add);
+    }
+    return room_list;
+}
+
+module.exports = { Price, pricesByDestination, priceByRoom }
--- routes/prices.js
+++ routes/prices.js
@@ -11,10 +11,25 @@ router.get('/destination/:destination_id/:checkin/:checkout/:lang/:currency/:gue
     const currency = req.params.currency;
     const guests = req.params.guests;
 
-    console.log(destination_id, checkin, checkout, lang, currency, guests);
+    //console.log(destination_id, checkin, checkout, lang, currency, guests);
     const partner_id = '1';
     //console.log(destination_id);
     const response = await priceModel.pricesByDestination(destination_id, checkin, checkout, lang, currency, guests, partner_id);
+    res.send(response);
+  });
+
+  router.get('/hotel/:hotel_id/:destination_id/:checkin/:checkout/:lang/:currency/:guests', async function(req, res, next) {
+    // example: localhost:3000/prices/hotel/rqRC/A0HL/2024-12-25/2025-01-07/en_US/SGD/1
+    const hotel_id = req.params.hotel_id;
+    const destination_id = req.params.destination_id;
+    const checkin = req.params.checkin;
+    const checkout = req.params.checkout;
+    const lang = req.params.lang;
+    const currency = req.params.currency;
+    const guests = req.params.guests;
+    const partner_id = 1;
+
+    const response = await priceModel.priceByRoom(hotel_id, destination_id, checkin, checkout, lang, currency, guests, partner_id);
     res.send(response);
   });
 
