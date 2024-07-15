0f0dbd3ec7b62ac8ab832299e3b22573935347b5
Harsh-44 <harshhareendran58@gmail.com>
2024-07-16 01:40:10 +0800

    initial commit for search page/home page

---
+++ frontend/src/components/pages/Bookings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Bookings() {
+  return (
+    <>
+      <h1 className='bookings' style={{color: '#282c34'}}>Bookings here!</h1>
+    </>
+  );
+}
+
+export default Bookings;
---
+++ frontend/src/components/pages/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+
+
+export default function Register() {
+  return (
+    <>
+    <h1 className='register' style={{color: '#282c34'}}>Register here!</h1>
+    </>
+  )
+}
