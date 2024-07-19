63eee91cef66d7d72b876742a26ae3011f779a82
FooChuanTian <foochuantian@gmail.com>
2024-07-19 22:50:56 +0800

    Revert "Merge branch 'main' into chuantian"
    This reverts commit 5dd50233cb26f693aed16013cef1aa8579afafa7, reversing
    changes made to 4d0c8a6efe419c06547f4ac48fd20aa939731900.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,28 +9,31 @@ import {
 
 import Test from "./pages/test";
 import Hotel from "./pages/HotelInformation";
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "./components/Navbar"; 
-import Home from './components/pages/Home';
-import Register from './components/pages/Register';
-import SignIn from './components/pages/SignIn';
-import Bookings from './components/pages/Bookings';
-import Services from './components/pages/Services';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/bookings' element={<Bookings />} />
-        <Route path='/services' element={<Services />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Hotel />} />
+            <Route path="/about" element={<Test />} />
+            <Route
+                path="/events"
+                element={<Test />}
+            />
+            <Route
+                path="/annual"
+                element={<Test />}
+            />
+            <Route path="/team" element={<Test />} />
+            <Route path="/blogs" element={<Test />} />
+            <Route
+                path="/sign-up"
+                element={<Test />}
+            />
+        </Routes>
     </Router>
-  );
+);
 }
 
 export default App;
