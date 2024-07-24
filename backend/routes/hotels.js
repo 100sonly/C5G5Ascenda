7cb629ba1b0724619ace8da132177be664fab9c9
FooChuanTian <foochuantian@gmail.com>
2024-07-24 14:00:46 +0800

    refactored hotels.js

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,26 +1,18 @@
 const express = require('express');
 var router = express.Router();
 const hotelModel = require('../models/hotels.js');
+const hotelController = require('../controllers/hotels.js');
 
 router.get('/destination/:destination_id', async function(req, res, next) {
-  const destination_id = req.params.destination_id;
-  //console.log(destination_id);
-  const all = await hotelModel.findByDestination(destination_id);
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.send(all);
+  hotelController.getHotelsByDestination(req, res);
 });
 
 router.get('/hotel/:hotel_id', async function(req, res, next) {
-  const hotel_id = req.params.hotel_id;
-  //console.log(hotel_id);
-  const all = await hotelModel.findById(hotel_id);
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.send(all);
+  hotelController.getHotelInfo(req, res);
 });
 
 router.get('/', async function(req, res, next) {
-
-  res.send('respond with a resource');
+  hotelController.defaultResponse(req, res);
 });
 
 
