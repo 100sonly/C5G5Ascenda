0c26fd0c63427fa55212cfe029040572833fb580
FooChuanTian <foochuantian@gmail.com>
2024-08-05 20:51:33 +0800

    Send Email Backend complete

--- backend/models/email.js
+++ backend/models/email.js
@@ -7,22 +7,32 @@ const mailerSend = new MailerSend({
 
 const sentFrom = new Sender("c5g5_esctest_2024@trial-x2p0347jdxk4zdrn.mlsender.net", "TEST EMAIL C5G5 ESC");
 
-const recipients = [
-  new Recipient("chuantian_foo@mymail.sutd.edu.sg", "Your Client")
-];
+async function send(personalInfo, bookingsDetails, hotelDetails) {
+  const recipients = [new Recipient(personalInfo.emailAddress, "Your Client")];
+  subject = "Confirmed booking - " + bookingsDetails.hotelName;
+  text_body = 
+  `<p>Dear ${personalInfo.salutation} ${personalInfo.firstName} ${personalInfo.lastName},<p><br />
+  <p>Thank you for booking with Ascendas! The following are the details of your booking:</p>
+  <ul>
+    <li>Booking ID: ${bookingsDetails.bookingId}</li>
+    <li>Name of hotel: ${bookingsDetails.hotelName}</li>
+    <li>Hotel Address: ${bookingsDetails.hotelAddress}</li>
+    <li>Room booked: ${bookingsDetails.roomName}</li>
+    <li>Check In: ${bookingsDetails.checkIn.date}, ${bookingsDetails.checkIn.time}</li>
+    <li>Check Out: ${bookingsDetails.checkOut.date}, ${bookingsDetails.checkOut.time}</li>
+  </ul>
+  <p>We hope you enjoy your stay!</p>`
 
-async function send() {
-const emailParams = new EmailParams()
-  .setFrom(sentFrom)
-  .setTo(recipients)
-  .setSubject("This is a Subject")
-  .setHtml("<strong>AHHHHHHHHH</strong>")
-  .setText("This is the text content");
+  const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipients)
+    .setSubject(subject)
+    .setHtml(text_body);
 
-mailerSend.email
-	.send(emailParams)
-  .then((response) => console.log(response))
-  .catch((error) => console.log(error));
+  mailerSend.email
+	  .send(emailParams)
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
 };
 
-module.exports = {send}
+module.exports = { send }
--- backend/routes/email.js
+++ backend/routes/email.js
@@ -1,8 +1,13 @@
-const emailModel = require("../models/email.js");
+const emailController = require("../controllers/email.js");
 const express = require('express');
 var router = express.Router();
 
+router.post("/send", async (req, res) => {
+    emailController.sendEmail(req, res);
+}); 
 
-router.get("/send", emailModel.send);
+router.get('/', async function(req, res, next) {
+    emailController.defaultResponse(req, res);
+});
 
 module.exports = router;
